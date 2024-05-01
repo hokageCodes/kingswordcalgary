4fb5833b2d8b167e75b72d354a508f2de9ef789a
hokageCodes <hashsenju94@gmail.com>
2024-05-01 19:20:06 +0100

    worship

--- components/dropdown/Dropdown.jsx
+++ components/dropdown/Dropdown.jsx
@@ -6,8 +6,8 @@ export default function Dropdown({ isOpen, toggle }) {
         <div className={`origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none ${isOpen ? 'block' : 'hidden'}`}>
           <div className="py-1">
             <Link href="#" className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100">Calgary</Link>
-            <Link href="#" className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100">Toronto</Link>
             <Link href="#" className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100">Vancuover</Link>
+            <Link href="#" className="text-gray-700 block px-4 py-2 text-sm hover:bg-gray-100">Toronto</Link>
           </div>
         </div>  
       );
